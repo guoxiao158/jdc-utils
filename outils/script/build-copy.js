71412682290d7ecb0cf37b9b69c93e426629e010
guoxiaoxiao8 <guoxiaoxiao8@jd.com>
2020-07-15 11:17:28 +0800

    fix：1

--- outils/script/build-copy.js
+++ outils/script/build-copy.js
@@ -11,15 +11,15 @@ const rootPath = path.resolve(__dirname, '../')
 let copying = ora('copying...')
 copying.start()
 rm('*.js', err => {
-  if (err) throw (err)
-  let folderList = fs.readdirSync(path.resolve(rootPath, 'src'))
-  folderList.forEach((item, index) => {
-    copy(`src/${item}/*.js`, rootPath, function (err, files) {
-      if (err) throw err;
-      if (index === folderList.length - 1) {
-        console.log(chalk.cyan('  Copy complete.\n'))
-        copying.stop()
-      }
+    if (err) throw (err)
+    let folderList = fs.readdirSync(path.resolve(rootPath, 'src'))
+    folderList.forEach((item, index) => {
+        copy(`src/${item}/*.js`, path.resolve(rootPath, 'dist'), function(err, files) {
+            if (err) throw err;
+            if (index === folderList.length - 1) {
+                console.log(chalk.cyan('  Copy complete.\n'))
+                copying.stop()
+            }
+        })
     })
-  })
 })
